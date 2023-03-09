bf13b3b1b0b90744534ecc305c0e173192624962
Talha Sheikh <121301354+TalhSheikh@users.noreply.github.com>
2023-03-09 18:41:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const eye = document.getElementById('eyeball');
+
+eye.addEventListener('click', e => {
+  e.preventDefault();
+  document.body.classList.toggle('show-password');
+  passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password'
+  passwordInput.focus();
+});
